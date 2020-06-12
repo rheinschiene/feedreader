10ff5f8fa422bf47665b63880edfd0a1ac99dfa9
rheinschiene <github@ms86.de>
2020-06-12 16:03:21 +0200

    mysql settings

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
 	'class' => 'yii\db\Connection',
-    	'dsn' => 'mysql:host=localhost;dbname=yii2',
-    	'username' => getenv("dbuser"),
-    	'password' => getenv("dbpw"),
+    	'dsn' => 'mysql:host=127.0.0.1;dbname=' . getenv("MYSQL_DATABASE"),
+    	'username' => 'root',
+    	'password' => getenv("MYSQL_ROOT_PASSWORD"),
     	'charset' => 'utf8',
 
     // Schema cache options (for production environment)
