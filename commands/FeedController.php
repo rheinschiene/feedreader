9e1e9b2d141403a7760ffac39593f2783a0320c1
rheinschiene <github@ms86.de>
2020-08-17 16:00:57 +0200

    Scharfes S vergessen.

--- commands/FeedController.php
+++ commands/FeedController.php
@@ -238,13 +238,15 @@ class FeedController extends Controller
 					$nodeValue = str_replace("Ö","O_e",$nodeValue);
 					$nodeValue = str_replace("ä","a_e",$nodeValue);
 					$nodeValue = str_replace("Ä","A_e",$nodeValue);
-		            $sanitized_nodeValue = filter_var($nodeValue, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
-		            $nodeValue = str_replace("u_e","ü",$sanitized_nodeValue);
+					$nodeValue = str_replace("ß","_ss_",$nodeValue);
+		            $nodeValue = filter_var($nodeValue, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
+		            $nodeValue = str_replace("u_e","ü",$nodeValue);
 					$nodeValue = str_replace("U_e","Ü",$nodeValue);
 					$nodeValue = str_replace("o_e","ö",$nodeValue);
 					$nodeValue = str_replace("O_e","Ö",$nodeValue);
 					$nodeValue = str_replace("a_e","ä",$nodeValue);
 					$nodeValue = str_replace("A_e","Ä",$nodeValue);
+					$nodeValue = str_replace("_ss_","ß",$nodeValue);
 					
 		            // Ist die Node selbst fett formatiert?
 		            if($node->tagName == "h1" || $node->tagName == "h2" || $node->tagName == "h3" || $node->tagName == "b" || $node->tagName == "strong") {
